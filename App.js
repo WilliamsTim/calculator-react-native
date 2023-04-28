28e2797cb2031e4c8f97c4e2744c8871af24b0e9
timothy <williamstim146@gmail.com>
2023-04-28 18:24:37 -0500

    Begin working on basic calculator outline and look. Create basic styles to be easily changed later and create all necessary buttons.

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.Column}>
+        <View style={styles.Row}>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+        </View>
+        <View style={styles.Row}>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+        </View>
+        <View style={styles.Row}>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+        </View>
+        <View style={styles.Row}>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+        </View>
+        <View style={styles.Row}>
+          <Pressable style={styles.lastCalc}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+          <Pressable style={styles.calcButtons}><Text>AC</Text></Pressable>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +47,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Row: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  Column: {
+    height: '57.69%',
+    justifyContent: 'space-evenly',
+  },
+  calcButtons: {
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    height: 70,
+    width: 70,
+    color: 'black',
+  },
+  lastCalc: {
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    height: 70,
+    width: 155,
+    color: 'black',
   },
 });
